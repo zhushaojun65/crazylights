2fee3e6d2178f61380cd7e017392a98b77f20d9d
lightsever@gmail.com <lightsever@gmail.com@c8a8923d-2d54-0410-bfec-218b41210122>
2014-06-14 06:20:58 +0000

    else 不执行问题 for 作用域问题

--- CLReach/CSLightStudio/CSLight/CSLight/Expression/Loop/CLS_Expression_LoopIf.cs
+++ CLReach/CSLightStudio/CSLight/CSLight/Expression/Loop/CLS_Expression_LoopIf.cs
@@ -8,7 +8,7 @@ namespace CSLight
     {
         public CLS_Expression_LoopIf()
         {
-           listParam= new List<ICLS_Expression>();
+            listParam = new List<ICLS_Expression>();
         }
         //Block的参数 一个就是一行，顺序执行，没有
         public List<ICLS_Expression> listParam
@@ -22,11 +22,22 @@ namespace CSLight
             ICLS_Expression expr_if = listParam[0];
             bool bif = (bool)expr_if.ComputeValue(content).value;
             //if (expr_init != null) expr_init.ComputeValue(content);
-            ICLS_Expression expr_go = listParam[1];
-            if(bif&&expr_go!=null)
+            ICLS_Expression expr_go1 = listParam[1];
+            ICLS_Expression expr_go2 = null;
+            if(listParam.Count>2)expr_go2= listParam[2];
+            if (bif && expr_go1 != null)
             {
-                return expr_go.ComputeValue(content);
-               
+
+                return expr_go1.ComputeValue(content);
+
+
+            }
+            else if (!bif && expr_go2 != null)
+            {
+
+                return expr_go2.ComputeValue(content);
+
+
             }
 
             //while((bool)expr_continue.value);
@@ -38,7 +49,7 @@ namespace CSLight
             return null;
         }
 
-     
+
         public override string ToString()
         {
             return "If|";
